0748563977f3f7a545834f311339de213c34d630
Tomás Sarmiento <t.sarmiento01@ufromail.cl>
2024-03-30 23:19:22 -0300

    docerfile

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+FROM golang:latest
+
+WORKDIR /app
+
+COPY go.mod ./
+COPY go.sum ./
+
+COPY . ./
+
+RUN go build -o /pricefetcher
+
+EXPOSE 3000
+
+CMD [ "/pricefetcher"]
--- main.go
+++ main.go
@@ -1,23 +1,18 @@
 package main
 
 import (
-	"context"
 	"flag"
-	"fmt"
-	"log"
-
-	"github.com/tomassar/crypto-price-fetcher-microservice/client"
 )
 
 func main() {
-	client := client.New("http://localhost:3000")
+	/* 	client := client.New("http://localhost:3000")
 
-	price, err := client.FetchPrice(context.Background(), "ET")
-	if err != nil {
-		log.Fatal(err)
-	}
+	   	price, err := client.FetchPrice(context.Background(), "ET")
+	   	if err != nil {
+	   		log.Fatal(err)
+	   	}
 
-	fmt.Printf("%+v\n", price)
+	   	fmt.Printf("%+v\n", price) */
 	listenAddr := flag.String("listenaddr", ":3000", "listen address, the service is running")
 	flag.Parse()
 
