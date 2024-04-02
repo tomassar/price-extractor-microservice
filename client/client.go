978828f50d02bac120ed6bf6b12bcddcb0bfd36b
Tomás Sarmiento <t.sarmiento01@ufromail.cl>
2024-04-01 22:01:38 -0300

    feat: gRPC client to connect to the server

--- client/client.go
+++ client/client.go
@@ -6,8 +6,21 @@ import (
 	"fmt"
 	"net/http"
 
+	"github.com/tomassar/crypto-price-fetcher-microservice/proto"
 	"github.com/tomassar/crypto-price-fetcher-microservice/types"
+	"google.golang.org/grpc"
 )
+
+func NewGRPCClient(remoteAddr string) (proto.PriceFetcherClient, error) {
+	conn, err := grpc.Dial(remoteAddr, grpc.WithInsecure())
+	if err != nil {
+		return nil, err
+	}
+
+	c := proto.NewPriceFetcherClient(conn)
+
+	return c, nil
+}
 
 type Client struct {
 	endpoint string
--- main.go
+++ main.go
@@ -1,20 +1,45 @@
 package main
 
 import (
+	"context"
 	"flag"
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/tomassar/crypto-price-fetcher-microservice/client"
+	"github.com/tomassar/crypto-price-fetcher-microservice/proto"
 )
 
 func main() {
 	var (
 		jsonAddr = flag.String("json", ":3000", "listen address of the json transport")
 		grpcAddr = flag.String("grpc", ":4000", "listen address of the gRPC transport")
+		svc      = NewLoggingService(&priceFetcher{})
+		ctx      = context.Background()
 	)
 	flag.Parse()
 
-	svc := NewLoggingService(&priceFetcher{})
+	grpcClient, err := client.NewGRPCClient(":4000")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	go func() {
+		time.Sleep(3 * time.Second)
+		resp, err := grpcClient.FetchPrice(ctx, &proto.PriceRequest{
+			Ticker: "ETH",
+		})
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Printf("%+v\n", resp)
+	}()
+
+	go makeGRPCServerAndRun(*grpcAddr, svc)
 
 	jsonServer := NewJSONAPIServer(*jsonAddr, svc)
 	jsonServer.Run()
 
-	go makeGRPCServerAndRun(*grpcAddr, svc)
 }
